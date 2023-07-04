a115b5ac932a3d2564d27a372f967f3061a9f394
unknown <samlizzy353@gmail.com>
2023-07-04 22:22:29 +0100

    It works now!

--- Week 0/Pset 0/tip.py
+++ Week 0/Pset 0/tip.py
@@ -1,5 +1,5 @@
 def main():
-    #dollars = dollars_to_float(input("How much was the meal? "))
+    dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
@@ -8,15 +8,13 @@ def main():
 def dollars_to_float(d):
     space, payment = d.split('$')
     payment = float(payment)
-    print (payment)
+    return (payment)
 
 
 def percent_to_float(p):
     tip_percent = p.replace('%', ' ')
     tip_percent = float(tip_percent) * 0.01
-    print tip_percent
-    #payment = float(payment)
+    return tip_percent
     
-
 
 main()

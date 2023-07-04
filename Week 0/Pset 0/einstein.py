35c6b88a1781c8b9ab5222e94456a250b1830f7d
unknown <samlizzy353@gmail.com>
2023-07-04 22:02:42 +0100

    Done with einstein

---
+++ Week 0/Pset 0/einstein.py
@@ -0,0 +1,10 @@
+# You're not fat, just powerful!
+
+def main():
+    mass = int(input("Hit me with that weight! "))
+    energy = mass * ((3 * 10**8) ** 2)
+    print (energy)
+
+main()
+
+# On my way to becoming a master of relativity...

914ae5a09f0d335f6e409b1d31bb220fd39caf3e
unknown <samlizzy353@gmail.com>
2023-07-16 12:52:05 +0100

    Implemented 12-hour logic but output overaccomodates (+8:01-8:59)

--- Week 1/Lecture 1/Meal Time/meal.py
+++ Week 1/Lecture 1/Meal Time/meal.py
@@ -1,15 +1,19 @@
 def main():
     time = input("What is the time? ")
-    converted_time = convert(time)
+
+    if time.endswith('a.m.') or time.endswith('p.m.'):
+        converted_time = new_convert(time)
+    else:
+        converted_time = convert(time)
 
     hour, minutes = converted_time.split('.')
 
     match hour:
-        case '7' | '8':
+        case '7':
             print ("Time for breakfast")
-        case '12' | '13':
+        case '12':
             print ("Time for lunch")
-        case '18' | '19':
+        case '18':
             print ("Time for dinner")
         case _:
             print ("It's not time for any meal!")
@@ -21,12 +25,24 @@ def convert(time):
     hour = int (hour)
 
     decimal = (f"{hour + (minute/60)}")
+    print (decimal)
     return decimal
+
+def new_convert (time):
+    time_stamp, meridian = time.split(' ')
+    hour, minute = time_stamp.split(':')
+    minute = int(minute)
+    hour = int (hour)
+
+    if meridian == 'p.m.':
+        hour += 12
+
+    decimal = (f"{hour + (minute/60)}")
+    print (decimal)
+    return decimal
+
 
 
 if __name__ == "__main__":
     while True:
         main()
-
-
-

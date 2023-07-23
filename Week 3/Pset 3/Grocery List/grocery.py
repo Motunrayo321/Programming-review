7d64a85edc61b04411f29f9ec0bf9eae8a21f901
unknown <samlizzy353@gmail.com>
2023-07-23 17:42:39 +0100

    Done with Grocery List

--- Week 3/Pset 3/Grocery List/grocery.py
+++ Week 3/Pset 3/Grocery List/grocery.py
@@ -1,14 +1,26 @@
 def main():
     my_list = {}
 
-    item = input("What's in your grocery list? ")
+    try:
+        while True:
 
-    if item in my_list:
-        my_list[item] += 1
+            item = input("What's in your grocery list? ")
+
+            if item in my_list:
+                my_list[item] += 1
+            
+            else:
+                my_list[item] = 1
+        
+    except (EOFError, KeyboardInterrupt):
+        print ("List complete!")
+
+    finally:
+        #print (my_list)
+
+        for thing in sorted(my_list.keys()):
+            print (f"{my_list[thing]} {thing.upper()}")
+
+
     
-    else:
-        my_list[item] = 1
-
-    
-
 main()

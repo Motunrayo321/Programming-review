f25f82eff5332174707d5bca97e0653c61082e58
unknown <samlizzy353@gmail.com>
2023-07-23 15:02:17 +0100

    Done with Nutrition Facts

---
+++ Week 2/Lecture 2/Nutrition Facts/nutrition.py
@@ -0,0 +1,33 @@
+def main():
+    fruit = input("Which fruit would you like to check? ").lower()
+    print (f"{fruit}: {calorie_check(fruit)} kcal")
+
+def calorie_check(fruit):
+
+    fruits = {
+        'apple': 130,
+        'avocado': 50,
+        'banana': 110,
+        'cantaloupe': 50,
+        'grapefruit': 60,
+        'grapes': 90,
+        'honeydew lemon': 50,
+        'kiwifruit': 90,
+        'lemon': 15,
+        'lime': 20,
+        'nectarine': 60,
+        'orange': 80,
+        'peach': 60,
+        'pear': 100,
+        'pineapples': 50,
+        'plums': 70,
+        'strawberries': 50,
+        'sweet cherries': 100,
+        'tangerine': 50,
+        'watermelon': 80
+        }
+    
+    if fruit in fruits:
+        return fruits[fruit]
+
+main()

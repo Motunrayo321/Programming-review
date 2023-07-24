4f9d5a20aa2f208f4ea4e3b79d6b14bea5c9994e
unknown <samlizzy353@gmail.com>
2023-07-24 15:51:42 +0100

    Looks like i just had to upgrade my requests version. Json was fine.

--- Week 4/Pset 4/6. Bitcoin Price Index/bitcoin.py
+++ Week 4/Pset 4/6. Bitcoin Price Index/bitcoin.py
@@ -4,17 +4,29 @@ import sys
 
 def main():
     try:
-        num = sys.argv[1]
+        num = float(sys.argv[1])
 
-        value = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        real_value = value.json()['bpi']
+        initial_price = check_price('USD')
+        final_price = num * initial_price
 
-        print (real_value)
+        print (f"${final_price:,.4f}")
 
     except ValueError:
         sys.exit("Invalid prompt!")
 
+
+def check_price(currency):
+    try:
+        value = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        total = json.dumps(value.json(), indent=2)
+        real_value = float(value.json()['bpi'][currency]['rate_float'])
+
+        print (total)
+        #print (real_value)
+        return real_value
+
     except requests.RequestException:
-        ...
+        sys.exit("No connection!")
+
 
 main()

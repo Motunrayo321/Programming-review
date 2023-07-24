e598ae3367195441bbb3c5a4959dd89bfc48d77c
unknown <samlizzy353@gmail.com>
2023-07-24 13:16:52 +0100

    Looks like I wasn't done, added level prompt for game

--- Week 4/Pset 4/4. Guessing Game/game.py
+++ Week 4/Pset 4/4. Guessing Game/game.py
@@ -1,8 +1,13 @@
 from random import randrange
 
 def main():
+    n = -1 
+
+    while type(n) != int and n < 0:
+        limit = int(input("What is the level of this game? "))
+
     guess = ''
-    num = randrange(10)
+    num = randrange(limit)
 
     while guess != num:
         guess = int(input("What is your guess? "))
@@ -13,6 +18,7 @@ def main():
             print ("Too large!")
         elif guess == num:
             print ("Just right!")
+
 
 
 main()

e304ae41aaece625b35760110ea19e391f78d923
unknown <samlizzy353@gmail.com>
2023-07-26 15:43:11 +0100

    Got the logic now. Time to read to file

--- Week 6/Pset 6/All problems/scourgify.py
+++ Week 6/Pset 6/All problems/scourgify.py
@@ -1,14 +1,41 @@
 from lines import argv_check
 from pizza import file_return
 import sys
+import json
 
 def main():
-        argv_check('csv', 2, all_file=True)
+    argv_check('csv', 2, all_file=True)
 
-    try:
-        header = ['name', 'house']
+    header = ['name', 'house']
 
-        file_name = sys.argv[1]
-        file = file_return(file_name, header)
+    before_file = sys.argv[1]
+    after_file = sys.argv[2]
 
+    prev_file = file_return(before_file, header)
+    cur_file = {}
+
+    #print (json.dumps(prev_file, indent=2))
+    print (len(prev_file))
+
+    for row in prev_file[1:]:
+        print (row)
+
+        first, last = row['name'].split(', ')
+
+        print(first + '\n' + last)
+
+        cur_file['first'] = first
+        cur_file['last'] = last
+        cur_file['house'] = row['house']
     
+    print ()
+    print (json.dumps(cur_file, indent=2))
+
+    # with open(before_file, 'a') as file:
+    #     writer = csv.DictWriter()
+
+    #     writer.writerow()
+
+
+if __name__ == "__main__":
+    main()

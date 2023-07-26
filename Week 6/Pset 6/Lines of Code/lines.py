64afe0a115277156e041da5b6e98f18d8acfe82d
unknown <samlizzy353@gmail.com>
2023-07-26 13:40:13 +0100

    Standard, but nonfunctional

--- Week 6/Pset 6/Lines of Code/lines.py
+++ Week 6/Pset 6/Lines of Code/lines.py
@@ -1,10 +1,22 @@
 import sys
 
-def line_check():
+def main():
     file_name = sys.argv[1]
+    file = line_check(file_name)
+    
+    print (len(file))
+
+def line_check(file_name):
+    actual_file = []
 
     with open(file_name, 'r') as file:
         for line in file:
-            print(line.strip())
+            print (line)
+            line = line.strip()
+            
+            if line and not line.startswith('#'):
+                print (line)
+                return(line)
 
-line_check()
+
+main()

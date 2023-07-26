d6c41cd280660216f17bc0e409fcc614b5c9256f
unknown <samlizzy353@gmail.com>
2023-07-26 13:39:34 +0100

    Still trying things out

--- Week 6/Pset 6/Lines of Code/lines.py
+++ Week 6/Pset 6/Lines of Code/lines.py
@@ -1,14 +1,10 @@
 import sys
 
-
 def line_check():
-    actual_file = []
-
     file_name = sys.argv[1]
 
     with open(file_name, 'r') as file:
         for line in file:
             print(line.strip())
-
 
 line_check()

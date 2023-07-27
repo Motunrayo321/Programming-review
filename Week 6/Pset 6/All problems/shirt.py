051eeec5256cc8f0629958e027e32ebaba516512
unknown <samlizzy353@gmail.com>
2023-07-27 13:54:11 +0100

    Done with logic, time to tidy code

--- Week 6/Pset 6/All problems/shirt.py
+++ Week 6/Pset 6/All problems/shirt.py
@@ -9,29 +9,33 @@ import csv
 
 
 def main ():
-    argv_check(['image', 'jpg', 'jpeg', 'png'], 2, all_file=True)
+    argv_check(['image', 'jpg', 'jpeg', 'png'], 3, all_file=True)
 
     file = sys.argv[1]
-    shirt = sys.argv[2]
-    file_return(file, shirt)
+    new_file = sys.argv[2]
+    shirt = sys.argv[3]
+    file_return(file, shirt, new_file)
     
 
-def file_return(file_name, over_file):
+def file_return(file_name, over_file, new_file):
 
     try:
         with Image.open(file_name) as file:
             print(file.size)
             file.show()
 
-            ImageOps.fit(file, (600,600))
-            print(file.size)
-            file.show()
+            fit_file = ImageOps.fit(file, (600,600))
+            print(fit_file.size)
+            fit_file.show()
 
             with Image.open(over_file) as shirt_file:
                 print(shirt_file.size)
                 shirt_file.show()
 
-                #ImageOps.fit(file, (500,500))
+                fit_file.paste(shirt_file, shirt_file)
+                fit_file.show()
+
+                fit_file.save(new_file)
         
     except FileNotFoundError:
         sys.exit("This file does not exist in this directory!")

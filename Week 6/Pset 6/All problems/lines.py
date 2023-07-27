cddca023e59c24d3cce19d8ef0c612130306225d
unknown <samlizzy353@gmail.com>
2023-07-27 12:35:04 +0100

    Adjusted the argv_check function to take a wider and more defined input

--- Week 6/Pset 6/All problems/lines.py
+++ Week 6/Pset 6/All problems/lines.py
@@ -1,9 +1,9 @@
 import sys
-import json
+from os import path
 
 
 def main():
-    argv_check('py', 1)
+    argv_check(['Python', 'py'], 1)
 
     file_name = sys.argv[1]
     file = line_check(file_name)
@@ -11,28 +11,56 @@ def main():
     print (len(file))
 
 
-def argv_check(ext, file_no, all_file=False, location=1):
-
+def argv_check(file_type:list, file_no:int, all_file=False, location=1):
+    """ file_type should be a list with the file name being the first and the extensions as the other items
+    file_no should be the number of expected files
+    all_file should be true if all the files should have one of the extensions listed in the list
+    location should be given if only all_file is false to know which file(s) to check its extension
+    """
+    
     arguments = sys.argv
 
+    # Checking command line file numbers
+    
     if len(arguments) < (file_no + 1):
         sys.exit("Too few arguments")
 
     elif len(arguments) > (file_no + 1):
         sys.exit("Too many arguments")
 
+    # Checking file extension
+
+    ext = file_type[0]
+
+    if len(arguments) == 2:
+        file_name, file_path = path.splitext(arguments[1])
+
+        print (file_name, file_path)
+        print(f".{file_type[1]}")
+        if not file_path in f".{file_type[1]}":
+            sys.exit(f"1. Not a {ext.title()} file")
+
+    # If files should be of the same type...
+
     if all_file:
+        #print (arguments[1:])
         for i in arguments[1:]:
-            if not arguments[i].endswith(f".{ext}"):
-                sys.exit(f"Not a {ext} file")
-    
-    elif len(arguments) == 2:
-        if not arguments[1].endswith(f".{ext}"):
-                sys.exit(f"Not a {ext} file")
+            #print (i)
+            file_name, file_path = path.splitext(i)
+            mark, file_path = file_path.split('.')
+            #print (file_name, file_path)
+            #print (file_type[1:])
+            #print(f"\n.{file_type[1:]}")
+            if not file_path in file_type[1:]:
+                sys.exit(f"2. Not a {ext.title()} file")
+
+    #if files can be different types
 
     else:
-        if not arguments[location].endswith(f".{ext}"):
-                sys.exit(f"Not a {ext} file")
+        file_name, file_path = path.splitext(arguments[location])
+        mark, file_path = file_path.split('.')
+        if not file_path in file_type[1:]:
+                sys.exit(f"3. Not a {ext.title()} file")
 
 
 def line_check(file_name):

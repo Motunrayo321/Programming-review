00f972c02226f0dcb3d60d5f84b7a62c4dbe93ff
unknown <samlizzy353@gmail.com>
2023-07-30 12:06:50 +0100

    Done with logic. Time to implement user output

--- Week 7/Pset 7/Working 9 to 5/working.py
+++ Week 7/Pset 7/Working 9 to 5/working.py
@@ -22,10 +22,13 @@ def convert(s):
     # ([6][0]) Unnecessary
     #(([6][0]){2})
 
-    [AM|PM]
+    # [AM|PM] incorrect
+    (AM|PM)
+
+    ((([1][0-2])|([0]?[1-9]))(:([0-5][0-9]))? ([AM|PM]))
 
     """
-    if match := re.search(r"^((([1][0-2])|([0]?[1-9]))(:([0-5][0-9]))?)$", s, re.IGNORECASE):
+    if match := re.search(r"^((([1][0-2])|([0]?[1-9]))(:([0-5][0-9]))? (AM|PM))$", s, re.IGNORECASE):
         return (match.groups())
 
 

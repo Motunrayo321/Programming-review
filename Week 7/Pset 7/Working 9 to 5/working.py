56ebec52bd24b32077ebf72bf714be4ace794735
unknown <samlizzy353@gmail.com>
2023-07-30 11:59:38 +0100

    Done [9:00/9] part

---
+++ Week 7/Pset 7/Working 9 to 5/working.py
@@ -0,0 +1,36 @@
+import re
+import sys
+
+
+def main():
+    print(convert(input("Hours: ")))
+
+
+def convert(s):
+    """
+    ([0-1][0-2])
+    # ((?:[0-1][0-2]){2})
+    ([0]?[0-9])
+    #([0-1]?[0-9]{1})
+
+    (([1][0-2])|([0]?[1-9]))
+
+    ([0-5]?[0-9])
+    ([0-5][0-9])
+    (:[0-5]?[0-9])
+    #(:[0-5]?[0-9]{1})
+    # ([6][0]) Unnecessary
+    #(([6][0]){2})
+
+    [AM|PM]
+
+    """
+    if match := re.search(r"^((([1][0-2])|([0]?[1-9]))(:([0-5][0-9]))?)$", s, re.IGNORECASE):
+        return (match.groups())
+
+
+
+
+while True:
+    if __name__ == "__main__":
+        main()

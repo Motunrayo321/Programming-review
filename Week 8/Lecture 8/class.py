3ee028855404a59aa4dfd1a8ec601b1d0a79d66f
unknown <samlizzy353@gmail.com>
2023-07-30 18:08:19 +0100

    Used properties to prevent variable override

--- Week 8/Lecture 8/class.py
+++ Week 8/Lecture 8/class.py
@@ -9,14 +9,28 @@ class Student():
     def __str__(self):
         return (f"name: {self.name}, house: {self.house}")
 
+
+    #getter
+    @property
+    def name(self):
+        return self._name
+    
+    #setter
+    @name.setter
+    def name(self, name):
+        if name == 'Homie':
+            raise ValueError ("That's actually not my real name!")
+        self._name = name
+
+
     def call(self):
-        print(self.name, self.house)
+        #print(self.name, self.house)
         print (self)
 
 def main():
     student = Student("Motun", "Shobande")
     student.name = 'Homie'
     student.call()
-    print (student)
+    #print (student)
 
 main()

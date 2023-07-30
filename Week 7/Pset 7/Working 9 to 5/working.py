0f46d8951ee226fea78feca981a86e12f7dc5086
unknown <samlizzy353@gmail.com>
2023-07-30 13:07:15 +0100

    Done with full implemtation. Time to clean up

--- Week 7/Pset 7/Working 9 to 5/working.py
+++ Week 7/Pset 7/Working 9 to 5/working.py
@@ -4,6 +4,7 @@ import sys
 
 def main():
     print(convert(input("Hours: ")))
+    pass
 
 
 def convert(s):
@@ -29,8 +30,43 @@ def convert(s):
 
     """
     if match := re.search(r"^(((?:[1][0-2])|(?:[0]?[1-9]))(?::([0-5][0-9]))? (AM|PM)) to (((?:[1][0-2])|(?:[0]?[1-9]))(?::([0-5][0-9]))? (AM|PM))$", s, re.IGNORECASE):
-        return (match.groups())
+        
+        start, end = match.group(1), match.group(5)
 
+        start_dets = {
+            'hour': match.group(2),
+            'minute': match.group(3),
+            'period': match.group(4)
+            }
+
+        end_dets = {
+            'hour': match.group(6),
+            'minute': match.group(7),
+            'period': match.group(8)
+            }
+
+        print (f"{start} - {end}\n")
+        print (start_dets)
+        print (end_dets)
+        print ()
+
+        final = output ([start_dets, end_dets])
+        return final
+
+
+def output(working_time):
+    for time in working_time:
+        if time['period'].lower() == 'pm':
+            time['hour'] = int(time['hour'])
+            time['hour'] += 12
+    
+        if not time['minute']:
+            time['minute'] = '00'
+    
+    start = working_time[0]
+    end = working_time[1]
+
+    return (f"{start['hour']}:{start['minute']} to {end['hour']}:{end['minute']}")
 
 
 
